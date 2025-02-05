5430549816605cfed1519ec93d873e4213b7d004
jhp0714 <gowns171@naver.com>
2025-02-06 01:33:06 +0900

    db 설정

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -4,3 +4,14 @@ ALLOWED_HOSTS = ['3.36.39.13']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 DEBUG = False
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'pybo',
+        'USER': 'dbmasteruser',
+        'PASSWORD': '6V:0{KMDYx)c6H^G&PdiR{k.*1;EZgd)',
+        'HOST': 'ls-e368986d13304641e3a0698314ca7aec99218605.crskikqoqlis.ap-northeast-2.rds.amazonaws.com',
+        'PORT': '5432',
+    }
+}
